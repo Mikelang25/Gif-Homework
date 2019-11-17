a14a8fc0498ca40f5a74f5d0ccb44ba29bf43ab6
unknown <michaellang2525@yahoo.com>
2019-11-16 22:13:04 -0500

    dynamically added gifs are now clickable and generate as expected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,14 +15,15 @@ function createButtons(){
         $("#buttons-main").append(b);
     }
 
+
+}
     $("#btn-create").on("click", function() {
         var a =$("#subject-box").val().trim();
         topics.push(a);
         createButtons();
     });
-}
 
-    $(".topic").on("click", function() {
+    function locateGifs() {
         var topicChosen = $(this).attr("data-subject")
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +topicChosen +"&api_key=p3nITdKm6Y1QRGMwkxH7z3dv2c11jB7w&limit=10";
 
@@ -33,13 +34,27 @@ function createButtons(){
             console.log(response);
             var results = response.data;
 
-            for(var j=0; j<results.length;i++){
-
-                
+            for(var j=0; j<results.length;j++){
+               var newGif = $("<div>"); 
+               newGif.addClass("gif-container");
+               gifImage = $("<img>");
+               gifImage.addClass("gif");
+               gifImage.attr("src",results[j].images.fixed_height_still.url);
+               gifImage.attr("gif-still",results[j].images.fixed_height_still.url);
+               gifImage.attr("gif-animate",results[j].images.fixed_height.url);
+               var t = $("<p>");
+               t.text(results[j].title);
+               var r = $("<p>");
+               r.text("Rating: " + results[j].rating);
+               newGif.append(gifImage);
+               newGif.append(t);
+               newGif.append(r);
+               $("#gifs-main").prepend(newGif);
             }
         })
 
-    });
+    }
 
+    $(document).on("click", ".topic", locateGifs);
 
 });
