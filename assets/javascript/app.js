298cd2442d70152e53e6ecc65989d5cb15088bc0
Mike Lang <michaellang2525@yahoo.com>
2019-11-18 17:16:17 -0500

    updated code to look at the radio button clicked to determine if request is for gift or movie

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,34 +30,56 @@ function createButtons(){
 
     function locateGifs() {
         var topicChosen = $(this).attr("data-subject")
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +topicChosen +"&api_key=p3nITdKm6Y1QRGMwkxH7z3dv2c11jB7w&limit=10";
+        
+        //determines if the user wants to search for a gif or movie related to the button that they are pushing
+        var searchType = $("input[name='search']:checked").val();
+        if(searchType ==="gif"){
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }) .then(function(response) {
-            console.log(response);
-            var results = response.data;
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +topicChosen +"&api_key=p3nITdKm6Y1QRGMwkxH7z3dv2c11jB7w&limit=10";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }) .then(function(response) {
+                console.log(response);
+                var results = response.data;
+    
+                for(var j=0; j<results.length;j++){
+                   var newGif = $("<div>"); 
+                   newGif.addClass("gif-container");
+                   gifImage = $("<img>");
+                   gifImage.addClass("gif");
+                   gifImage.attr("src",results[j].images.fixed_height_still.url);
+                   gifImage.attr("gif-still",results[j].images.fixed_height_still.url);
+                   gifImage.attr("gif-animate",results[j].images.fixed_height.url);
+                   gifImage.attr("state","still");
+                   var t = $("<p>");
+                   t.text(results[j].title);
+                   var r = $("<p>");
+                   r.text("Rating: " + results[j].rating);
+                   newGif.append(gifImage);
+                   newGif.append(t);
+                   newGif.append(r);
+                   $("#gifs-main").prepend(newGif);
+                }
+            });
 
-            for(var j=0; j<results.length;j++){
-               var newGif = $("<div>"); 
-               newGif.addClass("gif-container");
-               gifImage = $("<img>");
-               gifImage.addClass("gif");
-               gifImage.attr("src",results[j].images.fixed_height_still.url);
-               gifImage.attr("gif-still",results[j].images.fixed_height_still.url);
-               gifImage.attr("gif-animate",results[j].images.fixed_height.url);
-               gifImage.attr("state","still");
-               var t = $("<p>");
-               t.text(results[j].title);
-               var r = $("<p>");
-               r.text("Rating: " + results[j].rating);
-               newGif.append(gifImage);
-               newGif.append(t);
-               newGif.append(r);
-               $("#gifs-main").prepend(newGif);
-            }
-        })
+        }else if (searchType ==="movie"){
+            //code to search and return for the movie items 
+            var queryURL = "https://www.omdbapi.com/?t=" + topicChosen + "&apikey=trilogy";
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }) .then(function(response) {
+                console.log(response);
+                var results = response.data;
+
+                   
+                
+                $("#gifs-main").prepend();
+                
+            });
+        }
 
     }
 
